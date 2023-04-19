de2f35a2f73ee0431b9f5ce6d7dab7434b7b19ae
Agustin GImenez <agimenez@fie.undef.edu.ar>
2023-04-19 18:55:16 -0300

    Primer commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* 9 Defina una función que calcule la longitud de una cadena de caracteres.
+ */
+int longitudCadena(char cadena[]) {
+    int longitud = 0;
+    while (cadena[longitud] != '\0') {
+        longitud++;
+    }
+    return longitud;
+}
+
+int main() {
+    char str[] = "Hola mundo!";
+    int len = longitudCadena(str);
+    printf("La longitud de la cadena es : %d\n", len);
+    return 0;
+}
